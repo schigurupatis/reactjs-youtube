7371644d0ad0cc46ee00606df920a928d78ab586
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-10-24 11:18:10 +0530

    Firebase CI/CD Github Actions Deploy

--- src/components/Video.js
+++ src/components/Video.js
@@ -68,7 +68,7 @@ export const AddVideo = ({ info }) => {
     <div className="border border-gray-300">
       <Video info={info} />
       <h3 className="text-gray-500 text-sm text-center">
-        Sponsored - Add Video - testingksdfjksd
+        Sponsored - Add Video - Firebase CI/CD Github Actions
       </h3>
     </div>
   );
