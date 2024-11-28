74623eb77b285830e74a47466b554be6ee8419f7
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-11-28 13:03:06 +0530

    search api url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,4 +14,4 @@ export const YOUTUBE_VIDEOS_API =
   YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
