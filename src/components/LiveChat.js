fddb13f4c48244fe761903946379db29b5c6565f
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-11-28 12:43:09 +0530

    worked with live chat options - stage1 initilization

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 const LiveChat = () => {
   return (
-    <div>LiveChat will comes here soon</div>
+    <div className='chatContainer border p-3 rounded-md w-full h-screen overflow-y-auto'>
+      <div className='chatDiv'>
+        <div className='flex justify-between gap-3'>
+          <img src='' alt="userimg" />
+          <p className='userName m-0'>User Name 1</p>
+          <p className='userComment m-0'>Comment 1 Goes Here</p>
+        </div>
+        <div className='flex justify-between gap-3'>
+          <img src='' alt="userimg" />
+          <p className='userName m-0'>User Name 1</p>
+          <p className='userComment m-0'>Comment 1 Goes Here</p>
+        </div>
+        <div className='flex justify-between gap-3'>
+          <img src='' alt="userimg" />
+          <p className='userName m-0'>User Name 1</p>
+          <p className='userComment m-0'>Comment 1 Goes Here</p>
+        </div>
+        <div className='flex justify-between gap-3'>
+          <img src='' alt="userimg" />
+          <p className='userName m-0'>User Name 1</p>
+          <p className='userComment m-0'>Comment 1 Goes Here</p>
+        </div>
+        <div className='flex justify-between gap-3'>
+          <img src='' alt="userimg" />
+          <p className='userName m-0'>User Name 1</p>
+          <p className='userComment m-0'>Comment 1 Goes Here</p>
+        </div>
+      </div>
+      
+     
+    </div>
   )
 }
 
